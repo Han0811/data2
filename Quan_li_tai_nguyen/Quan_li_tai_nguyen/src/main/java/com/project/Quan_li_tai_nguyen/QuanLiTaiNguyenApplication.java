95fcd1048654dc37b86531a82242ae5cf5b4110f
Han0811 <huuan7968@gmail.com>
2024-10-25 22:11:34 +0700

    an

---
+++ Quan_li_tai_nguyen/Quan_li_tai_nguyen/src/main/java/com/project/Quan_li_tai_nguyen/QuanLiTaiNguyenApplication.java
@@ -0,0 +1,13 @@
+package com.project.Quan_li_tai_nguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLiTaiNguyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLiTaiNguyenApplication.class, args);
+	}
+
+}
---
+++ Quan_li_tai_nguyen/Quan_li_tai_nguyen/src/test/java/com/project/Quan_li_tai_nguyen/QuanLiTaiNguyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Quan_li_tai_nguyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLiTaiNguyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
